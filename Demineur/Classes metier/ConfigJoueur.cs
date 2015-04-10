43c8c6265a9c82afec62222bdef00885ba5e9d4f
Tri125 <kraig.tri125@gmail.com>
2015-04-10 14:06:02 -0400

    Classe de configuration des options
    -Classe ConfigJoueur pour les options du jeux.
    
    -Sérialisation pour l'enregistrement

---
+++ Demineur/Classes metier/ConfigJoueur.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Demineur
+{
+	[Serializable]
+	public class ConfigJoueur
+	{
+		[XmlElement("Mines de coins")]
+		public bool MinesCoins { get; set; }
+
+		[XmlElement("Taille des cases")]
+		public int TailleCases { get; set; }
+
+		public ConfigJoueur() { }
+
+		public ConfigJoueur(bool minesCoins, int tailleCases)
+		{
+			MinesCoins = minesCoins;
+			TailleCases = tailleCases;
+		}
+	}
+}
--- Demineur/MainWindow.xaml.cs
+++ Demineur/MainWindow.xaml.cs
@@ -12,20 +12,52 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace Demineur
 {
-    /// <summary>
-    /// Logique d'interaction pour MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
+	/// <summary>
+	/// Logique d'interaction pour MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
 
-            FenetreChampMines fenetreJeu = new FenetreChampMines(5, 5, 4);
-            gridPrincipale.Children.Add(fenetreJeu);
-        }
-    }
+			FenetreChampMines fenetreJeu = new FenetreChampMines(5, 5, 4);
+			gridPrincipale.Children.Add(fenetreJeu);
+
+			XmlTextWriter ecriveur = null;
+			XmlSerializer serializer = new XmlSerializer(typeof(ConfigJoueur));
+
+			ConfigJoueur c = new ConfigJoueur(true, 15);
+
+			try
+			{
+				ecriveur = new XmlTextWriter("yolo", null);
+				ecriveur.WriteStartDocument();
+				serializer.Serialize(ecriveur, c);
+				ecriveur.WriteEndDocument();
+			}
+
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+				return;
+			}
+
+			finally
+			{
+				if (ecriveur != null)
+				{
+					ecriveur.Close();
+				}
+			}
+
+
+
+		}
+	}
 }
