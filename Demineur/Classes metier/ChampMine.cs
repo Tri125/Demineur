0f1f8e0aa54fec74a800d190dcf43fd80908bc70
Tri125 <kraig.tri125@gmail.com>
2015-04-15 09:27:18 -0400

    Option de ne pas générer les mines dans les coins
    -Nouvelle méthode assignerMinesSansCoins de la classe ChampMine.
    Vérifie qu'aucune mine n'est généré dans les coins de la grille.

--- Demineur/Classes metier/ChampMine.cs
+++ Demineur/Classes metier/ChampMine.cs
@@ -19,7 +19,7 @@ namespace Demineur
         public ChampMines(int largeur, int hauteur, int nbMines)
         {
             if (largeur < 2 || hauteur < 2)
-            { 
+            {
                 throw new Exception("Un champ de mine ne peut avoir une dimension plus petite que 2 en largeur ou en hauteur.");
             }
 
@@ -38,7 +38,10 @@ namespace Demineur
         private void initialiserChampMines(int nbMines)
         {
             genererChampMines(LargeurChampMine, HauteurChampMine);
-            assignerMines(nbMines);
+            if (App.config.OptionUtilisateur.MinesCoins)
+                assignerMines(nbMines);
+            else
+                assignerMinesSansCoins(nbMines);
 
             lierZones();
 
@@ -51,7 +54,7 @@ namespace Demineur
         /// <param name="largeur">La largeur du champ de mine (axe X).</param>
         /// <param name="hauteur">Le hauteur du champ de mine (axe Y).</param>
         private void genererChampMines(int largeur, int hauteur)
-        { 
+        {
             List<List<Zone>> liste2D = new List<List<Zone>>();
 
             for (int i = 0; i < largeur; i++)
@@ -74,83 +77,108 @@ namespace Demineur
         /// À partir d'une liste de zones initialisée, assigne au hasard le nombre de mines demandées.
         /// </summary>
         private void assignerMines(int nbMines)
-        { 
+        {
             Random r = new Random(DateTime.Now.Millisecond);
             int x, y;
             Zone zoneCible;
             int minesGeneree = 0;
 
             while (minesGeneree < nbMines)
-            { 
+            {
                 x = r.Next(LstZones.Count);
                 y = r.Next(LstZones[0].Count);
 
                 zoneCible = LstZones[x][y];
 
                 if (!zoneCible.ContientMine)
-                { 
+                {
                     zoneCible.ContientMine = true;
-                    minesGeneree ++;
+                    minesGeneree++;
                 }
             }
 
+        }
+
+        private void assignerMinesSansCoins(int nbMines)
+        {
+            Random r = new Random(DateTime.Now.Millisecond);
+            int x, y;
+            Zone zoneCible;
+            int minesGeneree = 0;
+
+            while (minesGeneree < nbMines)
+            {
+                do
+                {
+                    x = r.Next(LstZones.Count);
+                    y = r.Next(LstZones[0].Count);
+                } while ( (x == 0 && y == 0) || (x == LstZones.Count -1 && y == 0) || (x == 0 && y == LstZones[0].Count - 1) || ( x == LstZones.Count() -1 && y == LstZones[0].Count() -1) );
+                zoneCible = LstZones[x][y];
+
+
+                if (!zoneCible.ContientMine)
+                {
+                    zoneCible.ContientMine = true;
+                    minesGeneree++;
+                }
+            }
         }
 
         /// <summary>
         /// Lie les zones voisines du champs de mines entres elles.
         /// </summary>
         private void lierZones()
-        { 
+        {
             // Faire les coins.
-            LstZones[0][0].assignerVoisins( null, null, null, null
+            LstZones[0][0].assignerVoisins(null, null, null, null
                                           , LstZones[1][0]
                                           , null
                                           , LstZones[0][1]
                                           , LstZones[1][1]
                                           );
-            LstZones[LargeurChampMine-1][0].assignerVoisins( null, null, null
-                                                           , LstZones[LargeurChampMine-2][0]
+            LstZones[LargeurChampMine - 1][0].assignerVoisins(null, null, null
+                                                           , LstZones[LargeurChampMine - 2][0]
                                                            , null
-                                                           , LstZones[LargeurChampMine-2][1]
-                                                           , LstZones[LargeurChampMine-1][1]
+                                                           , LstZones[LargeurChampMine - 2][1]
+                                                           , LstZones[LargeurChampMine - 1][1]
                                                            , null
                                                            );
-            LstZones[0][HauteurChampMine-1].assignerVoisins( null
-                                                           , LstZones[0][HauteurChampMine-2]
-                                                           , LstZones[1][HauteurChampMine-2]
+            LstZones[0][HauteurChampMine - 1].assignerVoisins(null
+                                                           , LstZones[0][HauteurChampMine - 2]
+                                                           , LstZones[1][HauteurChampMine - 2]
                                                            , null
-                                                           , LstZones[1][HauteurChampMine-1]
+                                                           , LstZones[1][HauteurChampMine - 1]
                                                            , null, null, null
                                                            );
-            LstZones[LargeurChampMine-1][HauteurChampMine-1].assignerVoisins( LstZones[LargeurChampMine-2][HauteurChampMine-2]
-                                                                            , LstZones[LargeurChampMine-1][HauteurChampMine-2]
+            LstZones[LargeurChampMine - 1][HauteurChampMine - 1].assignerVoisins(LstZones[LargeurChampMine - 2][HauteurChampMine - 2]
+                                                                            , LstZones[LargeurChampMine - 1][HauteurChampMine - 2]
                                                                             , null
-                                                                            , LstZones[LargeurChampMine-2][HauteurChampMine-1]
+                                                                            , LstZones[LargeurChampMine - 2][HauteurChampMine - 1]
                                                                             , null, null, null, null
                                                                             );
 
             // Faire la rangée du haut (moins les coins).
             for (int i = 1; i < LargeurChampMine - 1; i++)
-			{
-                LstZones[i][0].assignerVoisins( null
+            {
+                LstZones[i][0].assignerVoisins(null
                                               , null
                                               , null
-                                              , LstZones[i-1][0]
-                                              , LstZones[i+1][0]
-                                              , LstZones[i-1][1]
-                                              , LstZones[i]  [1]
-                                              , LstZones[i+1][1]
+                                              , LstZones[i - 1][0]
+                                              , LstZones[i + 1][0]
+                                              , LstZones[i - 1][1]
+                                              , LstZones[i][1]
+                                              , LstZones[i + 1][1]
                                               );
-			}
+            }
 
             // Faire la rangée du bas (moins les coins).
             for (int i = 1; i < LargeurChampMine - 1; i++)
             {
-                LstZones[i][HauteurChampMine -1].assignerVoisins( LstZones[i-1][HauteurChampMine-2]
-                                                                , LstZones[i]  [HauteurChampMine-2]
-                                                                , LstZones[i+1][HauteurChampMine-2]
-                                                                , LstZones[i-1][HauteurChampMine-1]
-                                                                , LstZones[i+1][HauteurChampMine-1]
+                LstZones[i][HauteurChampMine - 1].assignerVoisins(LstZones[i - 1][HauteurChampMine - 2]
+                                                                , LstZones[i][HauteurChampMine - 2]
+                                                                , LstZones[i + 1][HauteurChampMine - 2]
+                                                                , LstZones[i - 1][HauteurChampMine - 1]
+                                                                , LstZones[i + 1][HauteurChampMine - 1]
                                                                 , null
                                                                 , null
                                                                 , null
@@ -160,27 +188,27 @@ namespace Demineur
             // Faire la rangée de gauche (moins les coins).
             for (int j = 1; j < HauteurChampMine - 1; j++)
             {
-                LstZones[0][j].assignerVoisins( null
-                                              , LstZones[0][j-1]
-                                              , LstZones[1][j-1]
+                LstZones[0][j].assignerVoisins(null
+                                              , LstZones[0][j - 1]
+                                              , LstZones[1][j - 1]
                                               , null
                                               , LstZones[1][j]
                                               , null
-                                              , LstZones[0][j+1]
-                                              , LstZones[1][j+1]
+                                              , LstZones[0][j + 1]
+                                              , LstZones[1][j + 1]
                                               );
             }
 
             // Faire la rangée de droite (moins les coins).
             for (int j = 1; j < HauteurChampMine - 1; j++)
             {
-                LstZones[LargeurChampMine-1][j].assignerVoisins( LstZones[LargeurChampMine-2][j-1]
-                                                               , LstZones[LargeurChampMine-1][j-1]
+                LstZones[LargeurChampMine - 1][j].assignerVoisins(LstZones[LargeurChampMine - 2][j - 1]
+                                                               , LstZones[LargeurChampMine - 1][j - 1]
                                                                , null
-                                                               , LstZones[LargeurChampMine-2][j]
+                                                               , LstZones[LargeurChampMine - 2][j]
                                                                , null
-                                                               , LstZones[LargeurChampMine-2][j+1]
-                                                               , LstZones[LargeurChampMine-1][j+1]
+                                                               , LstZones[LargeurChampMine - 2][j + 1]
+                                                               , LstZones[LargeurChampMine - 1][j + 1]
                                                                , null
                                                                );
             }
@@ -190,14 +218,14 @@ namespace Demineur
             {
                 for (int j = 1; j < HauteurChampMine - 1; j++)
                 {
-                    LstZones[i][j].assignerVoisins( LstZones[i-1][j-1]
-                                                  , LstZones[i]  [j-1]
-                                                  , LstZones[i+1][j-1]
-                                                  , LstZones[i-1][j]
-                                                  , LstZones[i+1][j]
-                                                  , LstZones[i-1][j+1]
-                                                  , LstZones[i]  [j+1]
-                                                  , LstZones[i+1][j+1]
+                    LstZones[i][j].assignerVoisins(LstZones[i - 1][j - 1]
+                                                  , LstZones[i][j - 1]
+                                                  , LstZones[i + 1][j - 1]
+                                                  , LstZones[i - 1][j]
+                                                  , LstZones[i + 1][j]
+                                                  , LstZones[i - 1][j + 1]
+                                                  , LstZones[i][j + 1]
+                                                  , LstZones[i + 1][j + 1]
                                                   );
                 }
             }
