4900ebc63dd10a3174f8604cc8386958a7c5449d
Tristan <kraig.tri125@gmail.com>
2015-05-07 19:33:01 -0400

    Commentaires MainWindow

--- Demineur/MainWindow.xaml.cs
+++ Demineur/MainWindow.xaml.cs
@@ -26,12 +26,16 @@ namespace Demineur
             InitializeComponent();
         }
 
+        //  Lorsque le bouton de nouvelle partie est cliqué.
         private void btnNouvellePartie_Click(object sender, RoutedEventArgs e)
         {
+            //  Ouverture de la fenêtre de nouvelle partie en mode ShowDialog pour qu'elle garde le focus et empêcher de jouer dans la fenêtre principale.
             FenetreNouvellePartie fenPartie = new FenetreNouvellePartie();
-            //DialogResult
+            //  ShowDialog retourne uniquement lorsque la fenêtre est fermé
+            //  Value est le DialogResult de la fenêtre. Utilisé pour voir si le fenêtre à été annulé ou non.
             if (fenPartie.ShowDialog().Value)
             {
+                //  Récupère les propriétés de la fenêtre pour créer une nouvelle partie.
                 int largeur = fenPartie.Largeur;
                 int hauteur = fenPartie.Hauteur;
                 int nbrMines = fenPartie.NbrMines;
@@ -39,36 +43,60 @@ namespace Demineur
             }
         }
 
+        // Bouton de configuration
         private void btnConfiguration_Click(object sender, RoutedEventArgs e)
         {
             FenetreConfiguration fenConfig = new FenetreConfiguration();
+            // Pour que la fenêtre garde le focus et empêcher de jouer dans la fenêtre principale.
             fenConfig.ShowDialog();
         }
 
+        /// <summary>
+        /// Crée une nouvelle fenêtre FenetreChampMines et la rajoute dans la grille de jeu.
+        /// </summary>
+        /// <param name="largeur">Largeur du jeu</param>
+        /// <param name="hauteur">Hauteur du jeu</param>
+        /// <param name="nbrMines">Nombre de mines du jeu</param>
         private void NouvellePartie(int largeur, int hauteur, int nbrMines)
         {
+            // S'il y a déjà une partie en cours
             if (fenetreJeu != null)
             {
+                // On désinscrit des évênements et on remet par défaut le label du statut de la partie (faire disparaître).
                 fenetreJeu.Terminer -= new FenetreChampMines.PartieTermineEventHandler(ChangeLabelJeu);
                 fenetreJeu.Drapeau -= new FenetreChampMines.DrapeauEventHandler(OnChangementDrapeau);
                 DefautLabelJeu();
             }
+            // Pas besoin de vérifier s'il était déjà présent ou non.
             gridPrincipale.Children.Remove(fenetreJeu);
             fenetreJeu = new FenetreChampMines(largeur, hauteur, nbrMines);
+
+            // Inscription aux évênements de FenetreChampMines pour savoir lorsque la partie est terminé et lorsqu'un drapeau est enlevé/placé.
             fenetreJeu.Terminer += new FenetreChampMines.PartieTermineEventHandler(ChangeLabelJeu);
             fenetreJeu.Drapeau += new FenetreChampMines.DrapeauEventHandler(OnChangementDrapeau);
+
             gridPrincipale.Children.Add(fenetreJeu);
+
+            // Initialise le compteur de mines.
             indicateurMine.SetMineCount(nbrMines);
+            // On rend visible le compteur.
             indicateurMine.Visibility = System.Windows.Visibility.Visible;
         }
 
+        // Lorsque le bouton de partie rapide est appuyé.
         private void btnPartieRapide_Click(object sender, RoutedEventArgs e)
         {
+            // Crée une nouvelle partie selon les configurations personnalisées de l'utilisateur.
             NouvellePartie(App.config.OptionUtilisateur.Largeur, App.config.OptionUtilisateur.Hauteur, App.config.OptionUtilisateur.NombresMines);
+
+            //  TODO Créer une nouvelle partie selon les derniers paramètres utilisés.
         }
 
+        //  Change le label du statut de la partie selon si la partie est gagnée ou perdue.
         private void ChangeLabelJeu(object sender)
         {
+            // C'est juste mauvais parce que le sender de l'event est un bool à savoir si le joueur est mort ou non...
+            // TODO Faire un EventHandler qui respecte le principe de sender et d'EventArgs.
             if (sender is bool)
             {
                 bool joueurMort = (bool)sender;
@@ -86,22 +114,27 @@ namespace Demineur
             }
         }
 
+        // Lors d'une nouvelle partie, remet à neuf le label de statut de la partie.
         private void DefautLabelJeu()
         {
             lblPartie.Foreground = Brushes.Black;
             lblPartie.Content = "";
         }
 
+        //  Appel la méthode à l'indicateur de mines pour incrémenter son compteur
         private void RetraitDrapeau()
         {
             indicateurMine.IncrementeMine();
         }
 
+        //  Appel la méthode à l'indicateur de mines pour décrémenter son compteur
         private void RajoutDrapeau()
         {
             indicateurMine.DecrementeMine();
         }
 
+        // Selon DrapeauEventArgs qui indique si un drapeau a été rajouté ou retiré,
+        // appel les méthodes appropriées.
         private void OnChangementDrapeau(object sender, FenetreChampMines.DrapeauEventArgs e)
         {
             if (e == FenetreChampMines.DrapeauEventArgs.Rajout)
